0f60221aa34cbba80800b30a9bd691fdb51e86ff
crossbell8368 <godfather346@gmail.com>
2022-09-19 21:52:37 +0900

    Day1: add ex3
    add ex3 code

---
+++ java_class_offline/src/day1/ex3.java
@@ -0,0 +1,13 @@
+package day1;
+
+public class ex3 {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println(a);
+        // 선언한 정수형 변수a의 출력을 의미
+        System.out.println('a');
+        // 문자형 변수a의 출력을 의미
+        System.out.println("a");
+        // 하나의 문자a 이지만, ""로 묶여있기에 문자열로 인식하여 출력
+    }
+}
