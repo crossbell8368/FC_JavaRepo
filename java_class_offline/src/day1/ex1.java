5f053b0971dee829475ad12a855d08f53ca1e3cf
crossbell8368 <godfather346@gmail.com>
2022-09-19 21:27:59 +0900

    Day1: add ex1
    Edit java_test and add ex1 code

---
+++ java_class_offline/src/day1/ex1.java
@@ -0,0 +1,20 @@
+package day1;
+
+public class ex1 {
+    public static void main(String[] args){
+
+        // Java 출력테스트
+        System.out.println("Hello Java World - 1");
+        System.out.println("Hello Java World - 2");
+        System.out.println("Hello Java World - 3");
+        System.out.println("Hello Java World - 4");
+        System.out.println("Hello Java World - 5");
+
+        // java 출력함수 테스트
+        System.out.println("1");
+        System.out.print("2");
+        System.out.printf("3");
+        System.out.println();
+    }
+
+}
---
+++ java_class_offline/src/java_test.java
@@ -0,0 +1,8 @@
+public class java_test {
+        public static void main(String[] args) {
+            System.out.println("Hello Java World!");
+   }
+}
+
+
+//
